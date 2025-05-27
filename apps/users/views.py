009a27886affbb38af500526cbf8b8ae712466d5
ExoticOnix <mshapirovskyy@gmail.com>
2025-05-27 22:13:59 +0300

    Added code-server which uses auth request.

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse, HttpResponseForbidden
+from django.http import HttpResponse
 
 # Create your views here.
 def auth_request(request):
     if request.user.is_authenticated:
         return HttpResponse("Allowed", status=200)
     
-    return HttpResponseForbidden("Forbidden: You are not authenticated")
+    return HttpResponse("Forbidden: You are not authenticated", status=401)
