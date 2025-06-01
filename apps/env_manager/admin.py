46a52784ab4178aa9301451b393770656510bb35
ExoticOnix <mshapirovskyy@gmail.com>
2025-06-01 23:10:37 +0300

    made admin fields readonly for envs

--- apps/env_manager/admin.py
+++ apps/env_manager/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Environment
 
+@admin.register(Environment)
+class EnvironmentAdmin(admin.ModelAdmin):
+    # Display all fields as read-only
+    def get_readonly_fields(self, request, obj=None):
+        return [field.name for field in self.model._meta.fields]
 
-# Register your models here.
-admin.site.register(Environment)
+    # Disable the ability to add new instances
+    def has_add_permission(self, request):
+        return False
