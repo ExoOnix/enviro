e2e9f02600fbe2150feffcdad11ed5ef970c0ee3
ExoticOnix <mshapirovskyy@gmail.com>
2025-06-04 23:34:37 +0300

    remade tasks to put into just container creation to output ids faster

--- apps/env_manager/admin.py
+++ apps/env_manager/admin.py
@@ -9,5 +9,5 @@ class EnvironmentAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
---
+++ apps/env_manager/tasks/docker.py
@@ -0,0 +1,69 @@
+import docker
+from ..models import Environment
+from core.celery import app
+
+
+@app.task
+def start_container(environment_id):
+    
+    client = docker.from_env()
+
+    container = None
+    try:
+        environment = Environment.objects.get(id=environment_id)
+        env_id = environment.id
+        router_name = f"code-server-{env_id}"
+        service_name = f"code-server-{env_id}"
+        middleware_stripprefix = f"code-server-stripprefix-{env_id}"
+        middleware_forwardauth = f"code-server-forwardauth-{env_id}"
+        path_prefix = f"/environment/{env_id}"
+
+        container = client.containers.run(
+            image="codercom/code-server:latest",
+            command=["--auth=none"],
+            labels={
+                "traefik.enable": "true",
+                f"traefik.http.routers.{router_name}.rule": f"PathPrefix(`{path_prefix}`)",
+                f"traefik.http.routers.{router_name}.entrypoints": "web",
+                f"traefik.http.routers.{router_name}.service": service_name,
+                f"traefik.http.services.{service_name}.loadbalancer.server.port": "8080",
+                f"traefik.http.middlewares.{middleware_stripprefix}.stripprefix.prefixes": path_prefix,
+                f"traefik.http.routers.{router_name}.middlewares": f"{middleware_forwardauth},{middleware_stripprefix}",
+                f"traefik.http.middlewares.{middleware_forwardauth}.forwardauth.address": "http://django-docker:8000/auth/",
+                f"traefik.http.middlewares.{middleware_forwardauth}.forwardauth.trustForwardHeader": "true",
+                f"traefik.http.middlewares.{middleware_forwardauth}.forwardauth.authResponseHeaders": "Remote-User"
+            },
+            network="traefiknet",
+            restart_policy={"Name": "unless-stopped"},
+            detach=True
+        )
+        
+        environment.resource_id = container.id
+        environment.status = "running"
+        environment.save()
+        print(f"Container {container.name} started with ID {container.id}")
+    except Exception as e:
+        if container:
+            container.remove(force=True)
+        environment.delete()
+        raise e
+
+@app.task
+def delete_container(environment_id):
+    client = docker.from_env()
+    try:
+        environment = Environment.objects.get(id=environment_id)
+        container = client.containers.get(environment.resource_id)
+        
+        try:
+            container.stop()
+            container.remove()
+            environment.delete()
+        except docker.errors.NotFound:
+            print(f"Container with id {environment.resource_id} not found, removing Environment record.")
+            environment.delete()
+        except Exception as e:
+            print(f"Error removing environment: {e}")
+            raise e
+    except Exception as e:
+        raise e
