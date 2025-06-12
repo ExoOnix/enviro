65f7539c1083572fb61d41768dd3d78036a8f07f
ExoticOnix <72964425+ExoOnix@users.noreply.github.com>
2025-06-12 19:32:31 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
-CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS", "https://v8sk80gk08kkcskkwg84ksss.onixtech.org")
+CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_CSRF_TRUSTED_ORIGINS", ["https://v8sk80gk08kkcskkwg84ksss.onixtech.org"])
 
 # Auth
 AUTH_USER_MODEL = "users.CustomUser"
