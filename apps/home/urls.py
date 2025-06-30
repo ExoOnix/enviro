0cce7c2bea3bd961636e7b5299833052f5222cb5
ExoticOnix <mshapirovskyy@gmail.com>
2025-06-30 15:49:22 +0300

    added comparison table

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('compare', views.compare, name='compare')
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def compare(request):
+    return render(request, 'compare.html')
