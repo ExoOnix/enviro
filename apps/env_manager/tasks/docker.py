8c0de5bbfac1815e01fa387087e819e6c5902666
ExoticOnix <mshapirovskyy@gmail.com>
2025-07-21 14:18:44 +0300

    removed abs base path

--- apps/env_manager/tasks/docker.py
+++ apps/env_manager/tasks/docker.py
@@ -41,7 +41,7 @@ def create_container(environment_id):
         if environment.image == "codercom/code-server:latest":
             run_kwargs["command"] = [
                 "-c",
-                f"mkdir -p /home/coder/project && code-server /home/coder/project --bind-addr 0.0.0.0:8080 --disable-getting-started-override=true --auth=none --abs-proxy-base-path={path_prefix}"
+                "mkdir -p /home/coder/project && code-server /home/coder/project --bind-addr 0.0.0.0:8080 --disable-getting-started-override=true --auth=none"
             ]
             run_kwargs["entrypoint"] = "bash"
         if settings.DOCKER_RUNTIME != "default":
