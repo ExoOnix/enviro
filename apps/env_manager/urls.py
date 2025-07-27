3dff631bd6b0d83381f5b3a599d6204ce010be66
ExoOnix <mshapirovskyy@gmail.com>
2025-07-27 19:53:43 +0000

    Reapply "added subdomains handling"
    This reverts commit ad78ebea00a38a23b2bdbae3d647fc6413775c47.

--- apps/env_manager/urls.py
+++ apps/env_manager/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('environment/<int:number>/', views.waiting_room, name='waiting_room'),
+    path('', views.loading_room, name='loading_room'),
 ]
--- apps/env_manager/views.py
+++ apps/env_manager/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Environment
 from django.http import HttpResponseNotFound, HttpResponse
+from apps.home import views as home_views
+from django.conf import settings
+
 
 def waiting_room(request, number: int):
     env = Environment.objects.filter(pk=number).first()
@@ -10,3 +13,15 @@ def waiting_room(request, number: int):
         return HttpResponse("Environment is stopped")
     else:
         return render(request, 'waiting_room.html', {'environment': env})
+
+def loading_room(request):
+    host = request.get_host().split(':')[0]  # Remove port if present
+    hostname = settings.HOSTNAME
+
+    if host.endswith(hostname):
+        subdomain_part = host[:-len(hostname)].rstrip('.')
+        subdomain_levels = subdomain_part.split('.')
+        if subdomain_levels and subdomain_levels[-1].startswith('env-'):
+            return render(request, 'waiting_room.html')
+
+    return home_views.home(request)
