6c332fc01977b01d0a20973cd721349a9f1bd58f
ExoticOnix <mshapirovskyy@gmail.com>
2025-07-31 20:46:02 +0300

    Made each env have seperate networks

--- apps/env_manager/tasks/docker.py
+++ apps/env_manager/tasks/docker.py
@@ -1,7 +1,7 @@
 import docker
 from ..models import Environment
 from core.celery import app
-
+import uuid
 from django.conf import settings
 
 @app.task
@@ -15,9 +15,25 @@ def create_container(environment_id):
         env_id = environment.id
         router_name = f"code-server-{env_id}"
         service_name = f"code-server-{env_id}"
+        network_name = f"enviro_envnet-{env_id}"
+        container_name = f"code-server-{env_id}-{uuid.uuid4().hex[:6]}"
         middleware_stripprefix = f"code-server-stripprefix-{env_id}"
         middleware_forwardauth = f"code-server-forwardauth-{env_id}"
-
+        traefik_container_name = "traefik"
+        
+        try:
+            network = client.networks.get(network_name)
+            print(f"Network {network_name} already exists.")
+        except docker.errors.NotFound:
+            network = client.networks.create(network_name, driver="bridge", check_duplicate=True)
+            print(f"Created network {network_name}")
+            
+        traefik_container = client.containers.get(traefik_container_name)
+        connected_networks = traefik_container.attrs["NetworkSettings"]["Networks"]
+        if network_name not in connected_networks:
+            network.connect(traefik_container)
+            
+            
         routing_type = settings.ROUTING_TYPE
         if routing_type == "subpath":
             path_prefix = f"/environment/{env_id}"
@@ -47,9 +63,10 @@ def create_container(environment_id):
                 f"traefik.http.middlewares.{middleware_forwardauth}.forwardauth.authResponseHeaders": "Remote-User"
             }
         run_kwargs = dict(
+            name=container_name,
             image=environment.image,
             labels=labels,
-            network="onixenvnet",
+            network=network_name,
             restart_policy={"Name": "unless-stopped"},
             detach=True
         )
@@ -81,17 +98,44 @@ def delete_container(environment_id):
     try:
         environment = Environment.objects.get(id=environment_id)
         container = client.containers.get(environment.resource_id)
+        traefik_container_name = "traefik"
         
         try:
             if container.status == 'running':
                 container.kill()
             container.remove()
+            # Delete network if it exists
+            env_id = environment.id
+            network_name = f"enviro_envnet-{env_id}"
+            traefik_container = client.containers.get(traefik_container_name)
+            
+            network = client.networks.get(network_name)
+            connected_networks = traefik_container.attrs["NetworkSettings"]["Networks"]
+            if network_name in connected_networks:
+                network.disconnect(traefik_container, force=True)
+                print(f"Disconnected {traefik_container_name} from network {network_name}")
+            network.remove()
+            
             environment.delete()
         except Exception as e:
             print(f"Error removing environment: {e}")
             raise e
     except docker.errors.NotFound:
         print(f"Container with id {environment.resource_id} not found, removing Environment record.")
+        try:
+            traefik_container_name = "traefik"
+            env_id = environment.id
+            network_name = f"enviro_envnet-{env_id}"
+            traefik_container = client.containers.get(traefik_container_name)
+            
+            network = client.networks.get(network_name)
+            connected_networks = traefik_container.attrs["NetworkSettings"]["Networks"]
+            if network_name in connected_networks:
+                network.disconnect(traefik_container, force=True)
+                print(f"Disconnected {traefik_container_name} from network {network_name}")
+            network.remove()
+        except Exception as e:
+            print(f"Error removing network: {e}")
         environment.delete()
     except Exception as e:
         raise e
